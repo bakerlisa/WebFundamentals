9724931d34bc6c0fcee79f121af2d3327cd33ec1
Lisa <lb.broadhead@gmail.com>
2022-01-28 18:25:59 -0600

    fizzbuzz assigment

--- JS/fizzbuzz/script.js
+++ JS/fizzbuzz/script.js
@@ -2,26 +2,26 @@ var cont = document.querySelector('.fizz-cont');
 
 for(i=1; i <= 100; i++){
     var newDiv = document.createElement("div");
+    var nameLoop;
 
     if(i % 3 == 0 && i % 5 == 0){
         console.log("FizzBuzz");
-        newDiv.textContent = "FizzBuzz";
-        newDiv.classList = "FizzBuzz";
-        cont.append(newDiv);
+        nameLoop = "FizzBuzz";
     }else if(i % 5 == 0){
         console.log("Buzz");
-        newDiv.textContent = "Buzz";
-        newDiv.classList = "buzz";
-        cont.append(newDiv);
+        nameLoop = "Buzz";
     }else if(i % 3 == 0 ){
         console.log("Fizz");
-        newDiv.textContent = "Fizz";
-        newDiv.classList = "fizz";
-        cont.append(newDiv);
+        nameLoop = "Fizz";
     }else{
         console.log(i);
-        newDiv.textContent = i;
-        newDiv.classList = "regular";
-        cont.append(newDiv);
+        nameLoop = "regular";
     }
+
+    newDiv.classList = nameLoop;
+    if(nameLoop == "regular"){
+        nameLoop = i;
+    }    
+        newDiv.textContent = nameLoop;
+    cont.append(newDiv);
 }
