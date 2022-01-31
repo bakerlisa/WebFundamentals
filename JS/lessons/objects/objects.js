c8b25076391db79b08101ab36d16fe80d42c77e2
Lisa <lb.broadhead@gmail.com>
2022-01-31 11:07:46 -0600

    notes on objects

---
+++ JS/lessons/objects/objects.js
@@ -0,0 +1,17 @@
+// JSON  = Java Script Object Notation
+var something = {
+    name : "Lisa"
+};
+
+console.log(something);
+something['address'] = '1234 N. 5678 S.';
+console.log(something.address);
+something.favnum = 99;
+something.favnum++;
+console.log(something.favnum);
+
+// Array: changes all the time, and they come in the same order every time
+// object: easier and orde is not important
+document.querySelector(".box").
+
+
