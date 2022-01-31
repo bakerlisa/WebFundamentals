320920cf334a9d496ef92e4104f1069247f5b36a
Lisa <lb.broadhead@gmail.com>
2022-01-31 15:27:25 -0600

    added a bunch of JS functionality in order for it to function as the assigment wants

---
+++ JS/buttonClicker/script.js
@@ -0,0 +1,28 @@
+function logStatus(e){
+    var currentValue = e.target.value;
+
+    if(currentValue == "Login"){
+        console.log(currentValue);
+        e.target.value = "Logout";
+    }else if(currentValue == "Logout"){
+        e.target.value = "Login";
+    }
+}
+
+function removeElement(e){
+    e.target.classList.add('hide');
+}
+
+function like(e){
+    // sets visual that you liked the definition
+    var thumbs = '<i class="fas fa-thumbs-up"></i>';
+    e.target.innerHTML += thumbs;
+    setTimeout(function(){
+        e.target.querySelector(".fa-thumbs-up").remove();
+    },3000);
+
+    //++1 visual like 
+    var likeElement = e.target.querySelector('.num');
+    var likeNumber = parseInt(likeElement.innerHTML)+ 1;
+    likeElement.innerHTML = likeNumber;
+}
