61deaa0313f3948ffd257edf43ab6e129187d5a6
Lisa <lb.broadhead@gmail.com>
2022-01-31 21:12:07 -0600

    tweaking the final code for the assigement

--- JS/likes/script.js
+++ JS/likes/script.js
@@ -1,18 +1,3 @@
-function logStatus(e){
-    var currentValue = e.target.value;
-
-    if(currentValue == "Login"){
-        console.log(currentValue);
-        e.target.value = "Logout";
-    }else if(currentValue == "Logout"){
-        e.target.value = "Login";
-    }
-}
-
-function removeElement(e){
-    e.target.classList.add('hide');
-}
-
 function like(e){
     // sets visual that you liked the definition
     var thumbs = '<i class="fas fa-thumbs-up"></i>';
