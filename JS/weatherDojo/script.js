eda343547048b9e04bdcdaa7b8ef3ea2e456adc3
Lisa <lb.broadhead@gmail.com>
2022-02-01 15:26:23 -0600

    adding the JS for the assigment

--- JS/weatherDojo/script.js
+++ JS/weatherDojo/script.js
@@ -8,3 +8,21 @@ function accept(){
     document.querySelector('.cookie-popup').remove();
 }
 
+// changes the temp from C / F
+function temp(value){
+    var allTemps = document.querySelectorAll('.temp');
+    var newTemp;
+
+    for(i=0;i < allTemps.length; i++){
+        console.log(allTemps[i].innerText);
+        if(value === 'f'){
+            newTemp = allTemps[i].innerText * 9/5 + 32;
+        }else if(value === 'c'){
+            newTemp = (5/9) * (allTemps[i].innerText - 32);
+        }
+        allTemps[i].innerText = Math.round(newTemp);
+        //replace the innerText of eaxh element
+    }
+
+}
+
