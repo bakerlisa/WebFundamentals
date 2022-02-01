7a686ee6a4614efbeaf44d1cca15d60c35ae8d0c
Lisa <lb.broadhead@gmail.com>
2022-02-01 15:03:38 -0600

    pretty much built the whole assigment

---
+++ JS/weatherDojo/script.js
@@ -0,0 +1,10 @@
+// Nav Click - weather report
+function weatherReport(){
+    alert("Loading weather report...");
+}
+
+// Cookie button ckick
+function accept(){
+    document.querySelector('.cookie-popup').remove();
+}
+
