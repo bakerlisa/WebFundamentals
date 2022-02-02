2a30087414e20f4f8f2bc00235865faf343b758c
Lisa <lb.broadhead@gmail.com>
2022-02-02 11:20:24 -0600

    got most of the functionality to work

--- JS/calculator/script.js
+++ JS/calculator/script.js
@@ -1,46 +1,73 @@
+// initially everything is zero
+// ********** VARS **********
+// as the user inputs numbers its added here
 var calcArr1 = [];
 var calcArr2 = [];
+//actual nums / opertors for problem saved
 var operatorArray = [];
+// clears the display
+var display = document.querySelector('#display');
 
+// ********** PRESS **********
 function press(button){
-    if(operatorArray.length > 1){
-        calcArr2  += button;
+    // operatorArray empty, add to val calcArr1
+    if(operatorArray.length === 0){
+        console.log(operatorArray.length === 0);
+        //keeps adding numbers to the end till we hit an operator
+        calcArr1 += button;
+        //what shows up visually on the screen
+        display.innerText = calcArr1;
     }else{
-        calcArr1  += button;
+        //else add to calcArr2
+        //add numbers on the end till we hit the equals button
+        calcArr2 += button;
+        //what shows up visually on the screen
+        display.innerText = `${calcArr1}${operatorArray[1]}${calcArr2} = `;
     }
+    
 }
 
+// ********** SETOP - operation **********
 function setOP(opt){
-    if(operatorArray.length < 2){
-        operatorArray.push(calcArr1);
-        operatorArray.push(opt);
-    }else{
-        alert("one math problem at a time");
+    //checks the first numbers placement as well as length to make sure its set
+    if(calcArr1.length > 0 && operatorArray.length < 1){
+        //pushes calcArr1 to operation array
+        operatorArray.splice(0,1,calcArr1);
+        //pushes in the operator at the right place
+        operatorArray.splice(1,1,opt);
+        display.innerText += opt;
     }
-
-    //add another statement in here that state if the lenth is == 3 than do the operation,
-    //reset the first value
-    //then do the operation as if the 3 number is the second number
-
-    //really the array will only ever be a lenght of three
 }
 
-// Calculates
+// ********** CALCULATES **********
 function calculate(){
     if(calcArr2.length > 0){
-        operatorArray.push(calcArr2);
-    }else{
-        alert('select a 2nd number');
-    }
+        operatorArray.splice(2,1,calcArr2);
+
+
     var operationClac = eval(parseInt(operatorArray[0]) + operatorArray[1]+parseInt(operatorArray[2]));
 
-    document.querySelector('#display').innerText = operationClac;
+    document.querySelector('#display').innerHTML = "";
+    document.querySelector('#display').innerHTML = ` ${operatorArray[0]}${operatorArray[1]}${operatorArray[2]} = <span> ${operationClac} </span>`;
+
+
+    // sets calcArr1 (which is always in the 0 position) to our total, then it clears eveything else. Length should be no longer than 3 at any given time
     calcArr1 = [];
     calcArr2 = [];
     operatorArray = [];
+    calcArr1.splice(0,1,operationClac);
+
+    }else{
+        // if there isn't a second value in the calcArr2[2] placement - don't do anything
+        return;
+    }    
 }
 
-// clears
+// ********** CLEARS **********
 function clr(){
-    document.querySelector("#display").innerText = 0;
+    document.querySelector("#display").innerHTML = "";
+    
+    calcArr1 = [];
+    calcArr2 = [];
+    operatorArray = [];
 }
